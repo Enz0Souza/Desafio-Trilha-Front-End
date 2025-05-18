073d8bdf504b24a6910d741074dde4419267664f
Enzo Souza <enzosouza20077@gmail.com>
2025-05-17 21:05:15 -0300

    implementação de login,lgpd e revisões no footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,10 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AgendaComponent } from './agenda/agenda.component';
 import { LoginComponent } from './login/login.component';
+import { ContatoComponent } from './contato/contato.component';
+import { CopyrightComponent } from './copyright/copyright.component';
 
 export const routes: Routes = [
-
-
     {
     path:'',
     redirectTo: 'home',
@@ -21,6 +21,12 @@ export const routes: Routes = [
   },
   {
     path:'login', component: LoginComponent
+  },
+  {
+    path:'contato', component: ContatoComponent
+  },
+  {
+    path:'copyright', component: CopyrightComponent
   },
 
 
